061866c86ba74c25ecb0046b3ca9e4e61e61b462
lockfilement <fer.ecu26@gmail.com>
2026-02-03 20:29:30 -0500

    .

--- src/types/tailwindcss.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "tailwindcss/lib/util/flattenColorPalette" {
-	type ColorPalette = Record<string, string | Record<string, string>>;
-	const flattenColorPalette: (colors: ColorPalette) => Record<string, string>;
-	export default flattenColorPalette;
-}
